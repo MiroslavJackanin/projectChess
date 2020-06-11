a80c20ddf820dd9bd56fdd18b540632933e2170e
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2020-06-11 15:48:39 +0200

    init

---
+++ src/chess/Chess.java
@@ -0,0 +1,90 @@
+package chess;
+
+public class Chess {
+    public String normalize(String position){
+        if(position==null || position.length()!=2 )
+            return null;
+
+        StringBuilder pos = new StringBuilder(position.toUpperCase());
+        char temp;
+        if ((int)pos.charAt(0) > 48 && (int)pos.charAt(0) < 57 && (int)pos.charAt(1) > 48 && (int)pos.charAt(1) < 57){
+            return null;
+        }else if ((int)pos.charAt(0) > 64 && (int)pos.charAt(0) < 73 && (int)pos.charAt(1) > 64 && (int)pos.charAt(1) < 73){
+            return null;
+        }else if ((int)pos.charAt(0) > 48 && (int)pos.charAt(0) < 57){
+            temp = pos.charAt(1);
+            pos.setCharAt(1, pos.charAt(0));
+            pos.setCharAt(0, temp);
+        }
+        System.out.println(pos);
+        return pos.toString();
+    }
+
+    public boolean checkBishop(String pos, String dest){
+        pos = normalize(pos);
+        dest = normalize(dest);
+        if(pos == null || dest == null)
+            return false;
+
+        return ((int) pos.charAt(0) - (int) dest.charAt(0)) == (Integer.parseInt(String.valueOf(pos.charAt(1))) - Integer.parseInt(String.valueOf(dest.charAt(1))));
+    }
+
+    public boolean checkKing(String pos, String dest){
+        pos=normalize(pos);
+        dest=normalize(dest);
+        if (pos==null|| dest==null){
+            System.out.println("1");
+            return false;
+        }
+        if (pos.equals(dest)){
+            System.out.println("2");
+            return false;
+        }
+        return Math.abs(pos.charAt(0) - dest.charAt(0)) <= Math.abs(1) && Math.abs(pos.charAt(1) - dest.charAt(1)) <= Math.abs(1);
+    }
+
+    public boolean checkQueen(String pos, String dest){
+        pos=normalize(pos);
+        dest=normalize(dest);
+        if (pos==null|| dest==null){
+            System.out.println("1");
+            return false;
+        }
+        if (pos.equals(dest)){
+            System.out.println("2");
+            return false;
+        }
+        return Math.abs(pos.charAt(0) - dest.charAt(0)) == Math.abs(pos.charAt(1) - dest.charAt(1))
+                || (pos.charAt(0) == dest.charAt(0) || pos.charAt(1) == dest.charAt(1));
+    }
+
+    public boolean checkRook(String pos, String dest){
+        pos=normalize(pos);
+        dest=normalize(dest);
+        if (pos==null|| dest==null){
+            System.out.println("1");
+            return false;
+        }
+        if (pos.equals(dest)){
+            System.out.println("2");
+            return false;
+        }
+        return pos.charAt(0) == dest.charAt(0) || pos.charAt(1) == dest.charAt(1);
+    }
+
+    public boolean checkKnight(String pos, String dest){
+        pos=normalize(pos);
+        dest=normalize(dest);
+        if (pos==null|| dest==null){
+            System.out.println("1");
+            return false;
+        }
+        if (pos.equals(dest)){
+            System.out.println("2");
+            return false;
+        }
+        int i = Math.abs(pos.charAt(0) - dest.charAt(0));
+        int j = Math.abs(pos.charAt(1) - dest.charAt(1));
+        return (i == 1 && j == 2) || (i == 2 && j == 1);
+    }
+}
---
+++ src/chess/Main.java
@@ -0,0 +1,8 @@
+package chess;
+
+public class Main {
+    public static void main(String[] args) {
+        Chess chess = new Chess();
+
+    }
+}
